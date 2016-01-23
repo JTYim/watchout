9347764be0593c4727efb38ac050f15aad32ff94
JT Yim <JT@JTYim.com>
2016-01-22 20:01:34 -0500

    1.Enemy SVG v1

--- client/watchout.js
+++ client/watchout.js
@@ -1,2 +1,19 @@
 // start slingin' some d3 here.
 
+// var BadGuys = function(){
+// 	this.name: name
+// })
+
+
+
+var width = 960;
+var height = 500;
+
+var svg = d3.select("body").append("svg")
+		.attr("width", width)
+		.attr("height", height)
+		// .attr("fill", backgrd)
+svg.append("ellipse")
+	.attr({ cx:33, cy:33, rx:15, ry:8, fill:"#333" });
+
+
