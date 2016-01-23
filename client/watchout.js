1199eba8bc62ef79e61c57195b781a3452af8039
JT Yim <JT@JTYim.com>
2016-01-22 22:48:00 -0500

    ...

--- client/watchout.js
+++ client/watchout.js
@@ -1,19 +1,51 @@
-// start slingin' some d3 here.
-
 // var BadGuys = function(){
-// 	this.name: name
-// })
+// 	this.name = name,
+// 	this.x = left,
+// 	this.y = top
+// };
+// BadGuys.prototype.move(){ }
+// var badGuys = new Array(30).map(function(e,i){ 
+// 	return { name: i } 
+// });
 
 
+// IIFE: Wrap entire thing? 
+// (function(){  })
 
-var width = 960;
-var height = 500;
+var badGuys = [];
+for(var i=1; i<=30; i++){
+	var badGuysObj ={ name: i } 
+	badGuys.push(badGuysObj)
+}	
 
+var width = 1920;
+var height = 1080;
 var svg = d3.select("body").append("svg")
 		.attr("width", width)
 		.attr("height", height)
-		// .attr("fill", backgrd)
-svg.append("ellipse")
-	.attr({ cx:33, cy:33, rx:15, ry:8, fill:"#333" });
+		.style("background-color", "black")
+		// .append("g")
+		// .attr("transform", "translate(" + 960 + "," + 500 + ")" );
+
+function update(zz){
+	var enemies = svg.selectAll("ellipse") 			
+		.data(zz)
+		.attr("class", "bad1")
+		.enter().append("ellipse") 					
+		.attr({ rx:15, ry:8, fill:"#330" })
+		.attr("cx", function(d,i){ return 0.9*Math.floor(Math.random()*width); })
+		.attr("cy", function(d,i){ return 0.9*Math.floor(Math.random()*height); })
+
+
+
+
+
+// transitions
+
+}
+update(badGuys);
+
+
+
 
 
