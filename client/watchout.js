e696e5b517daf4a21a34bf0bf3422a5a82f78ad2
JT Yim <JT@JTYim.com>
2016-01-23 16:35:48 -0500

    Collision detection contd

--- client/watchout.js
+++ client/watchout.js
@@ -12,57 +12,78 @@
 // IIFE: Wrap entire thing? 
 // (function(){  })
 
-var goodGuy=[{ 
-	name: 0, 
-	x:500, 
-	y:500 
-}];
-var badGuys = [];
-for(var i=1; i<=10; i++){
-	var badGuysObj ={ name: i } 
-	badGuys.push(badGuysObj)
-}	
-
-var width = 1920;
-var height = 1080;
-var svg = d3.select("body").append("svg")
+	var width = 1920;
+	var height = 1080;
+	var svg = d3.select("body").append("svg")
 		.attr("width", width)
 		.attr("height", height)
 		.style("background-color", "black")
 		// .append("g")
 		// .attr("transform", "translate(" + 960 + "," + 500 + ")" );
 
-function update(zz, yy){
-	var enemies = svg.selectAll("ellipse") 			
-		.data(zz)
-			.attr("class", "bad1")
-		.enter().append("ellipse") 					
-			.attr({ rx:15, ry:8, fill:"#320" })
-			.attr("cx", 50)
-			.attr("cy", 50);
-		// enemies.attr("x", function(d, i) { return 100; })
-		// enemies.exit().remove();
+	var goodGuy=[{ 
+		name: 0, 
+		x: (width*0.66), 
+		y:(height*0.66), 
+	}];
 
-	var drag = d3.behavior.drag().on('drag', function(){ 
-             	me.attr('cx', d3.event.x)
-			me.attr('cy', d3.event.y); 
-		})
+	var badGuys = [1,2,3,4,5,6,7,8,9,10];
+	// for(var i=1; i<=10; i++){
+	// 	badGuysObj ={ name: "name", info: "info" } 
+	// 	badGuys.push(badGuysObj)
+	// }	
+
+function update(zz, yy){
+
+	var enemies = svg.selectAll("ellipse")
+		.data(zz) 				
 	var me = svg.selectAll("circle")
 		.data(yy)
-	me.attr("class","good")
+
+	enemies.enter().append("ellipse") 
+		.attr({ rx:15, ry:8, fill:"red" })
+		.attr("cx", 50)
+		.attr("cy", 50)
+		.attr("class", "bad1")
 	me.enter().append("circle")
-			.attr({r:20, fill:"#350"})
-			.attr('cx', function(d) { return d.x; })
-			.attr('cy', function(d) { return d.y; })
-			.call(drag)
+		.attr({r:20, fill:"#350"})
+		.attr('cx', function(d) { return d.x; })
+		.attr('cy', function(d) { return d.y; })
+		.attr("class","good")
+
+	var move = d3.behavior.drag().on('drag', function(){ 
+           	me.attr('cx', d3.event.x);
+		me.attr('cy', d3.event.y);
+	})
+	me.call(move)
+	debugger; 			
+	enemies.exit().remove();
+	enemies.exit().remove();
 }
 
 update(badGuys, goodGuy);
 setInterval(function() {
-	update(d3.selectAll("ellipse").transition().duration(2000)
+	update(d3.selectAll("ellipse").transition().delay(50).duration(1000)
 		.attr("cx", function(d){ return 0.9*Math.floor(Math.random()*width); })
 		.attr("cy", function(d){ return 0.9*Math.floor(Math.random()*height); }))
 }, 1000);
+
+function collide(player, enemy){
+	debugger;
+	var distance = Math.sqrt( Math.pow(( player.attr("cx") - enemy.attr("cx"), 2) + Math.pow((player.cy-enemy.cy),2)) );	 
+	distance < 50 && (console.log("collision"));
+};
+
+setTimeout(function(){
+	svg.selectAll('ellipse').forEach(function(enemy){
+		collide(svg.selectAll('circle'), enemy);
+	})
+},500)
+
+
+
+
+
 
 
 
