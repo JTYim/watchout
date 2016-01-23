ede853299e2a56a0691e9c1267d5cfdfbef92118
JT Yim <JT@JTYim.com>
2016-01-23 00:22:53 -0500

    c- draggable player

--- client/watchout.js
+++ client/watchout.js
@@ -12,8 +12,13 @@
 // IIFE: Wrap entire thing? 
 // (function(){  })
 
+var goodGuy=[{ 
+	name: 0, 
+	x:500, 
+	y:500 
+}];
 var badGuys = [];
-for(var i=1; i<=30; i++){
+for(var i=1; i<=10; i++){
 	var badGuysObj ={ name: i } 
 	badGuys.push(badGuysObj)
 }	
@@ -27,26 +32,37 @@ var svg = d3.select("body").append("svg")
 		// .append("g")
 		// .attr("transform", "translate(" + 960 + "," + 500 + ")" );
 
-function update(zz){
+function update(zz, yy){
 	var enemies = svg.selectAll("ellipse") 			
 		.data(zz)
-		.attr("class", "bad1")
+			.attr("class", "bad1")
 		.enter().append("ellipse") 					
-		.attr({ rx:15, ry:8, fill:"#330" })
-		.attr("cx", 50)
-		.attr("cy", 50)
+			.attr({ rx:15, ry:8, fill:"#320" })
+			.attr("cx", 50)
+			.attr("cy", 50);
+		// enemies.attr("x", function(d, i) { return 100; })
+		// enemies.exit().remove();
 
-	// enemies.attr("x", function(d, i) { return 100; })
-	// debugger;
-	// enemies.exit().remove();
+	var drag = d3.behavior.drag().on('drag', function(){ 
+             	me.attr('cx', d3.event.x)
+			me.attr('cy', d3.event.y); 
+		})
+	var me = svg.selectAll("circle")
+		.data(yy)
+	me.attr("class","good")
+	me.enter().append("circle")
+			.attr({r:20, fill:"#350"})
+			.attr('cx', function(d) { return d.x; })
+			.attr('cy', function(d) { return d.y; })
+			.call(drag)
 }
-update(badGuys);
-setInterval(function() {
-	update(d3.selectAll("ellipse").transition()
-		.attr("cx", function(d,i){ return 0.9*Math.floor(Math.random()*width); })
-		.attr("cy", function(d,i){ return 0.9*Math.floor(Math.random()*height); }))
-}, 1000);
 
+update(badGuys, goodGuy);
+setInterval(function() {
+	update(d3.selectAll("ellipse").transition().duration(2000)
+		.attr("cx", function(d){ return 0.9*Math.floor(Math.random()*width); })
+		.attr("cy", function(d){ return 0.9*Math.floor(Math.random()*height); }))
+}, 1000);
 
 
 
